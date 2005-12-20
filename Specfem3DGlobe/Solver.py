bc5f9418d57cd901cbdf359d2806a3b584fe7413
Leif Strand <leif@geodynamics.org>
2005-12-20 04:17:08 +0000

    Command line overrides .cfg files, and not vice-versa. Allow users to give .cfg/.pml files on the command line. Handle more exceptions in _richCast() (bool, dimensional). Changed RECORD_LENGTH_IN_MINUTES to a dimensional.

--- Specfem3DGlobe/Solver.py
+++ Specfem3DGlobe/Solver.py
@@ -2,6 +2,7 @@
 
 
 from pyre.components.Component import Component
+from pyre.units.time import minute
 import Specfem3DGlobeCode
 
 
@@ -9,7 +10,7 @@ class Solver(Component):
     
     class Inventory(Component.Inventory):
 
-        from pyre.inventory import bool, float, int
+        from pyre.inventory import bool, dimensional, float, int
     
         MOVIE_SURFACE                 = bool("movie-surface")
         MOVIE_VOLUME                  = bool("movie-volume")
@@ -17,7 +18,7 @@ class Solver(Component):
         PRINT_SOURCE_TIME_FUNCTION    = bool("print-source-time-function")
         
         HDUR_MOVIE                    = float("hdur-movie")
-        RECORD_LENGTH_IN_MINUTES      = float("record-length")
+        record_length                 = dimensional("record-length", default=0.0*minute)
         
         NTSTEP_BETWEEN_FRAMES         = int("ntstep-between-frames")
         NTSTEP_BETWEEN_OUTPUT_INFO    = int("ntstep-between-output-info")
@@ -27,6 +28,12 @@ class Solver(Component):
         
     def __init__(self, name):
         Component.__init__(self, name, "solver")
+
+    def _init(self):
+        Component._init(self)
+
+        # convert to minutes
+        self.RECORD_LENGTH_IN_MINUTES = self.inventory.record_length / minute
 
 
 # end of file
