52aa8e58bf7e7e8c3fddf0b52d4f9e1c8c3cf3b8
Leif Strand <leif@geodynamics.org>
2006-01-18 03:42:22 +0000

    Reworked InputPile handling.

--- Specfem3DGlobe/Solver.py
+++ Specfem3DGlobe/Solver.py
@@ -12,7 +12,7 @@ class Solver(Component):
 
         from pyre.inventory import bool, dimensional, float, inputFile, int
     
-        CMTSOLUTION                   = inputFile("cmt-solution", default="DATA/CMTSOLUTION")
+        cmtSolution                   = inputFile("cmt-solution", default="DATA/CMTSOLUTION")
         
         MOVIE_SURFACE                 = bool("movie-surface")
         MOVIE_VOLUME                  = bool("movie-volume")
@@ -28,10 +28,12 @@ class Solver(Component):
         NUMBER_OF_RUNS                = int("number-of-runs")
         NUMBER_OF_THIS_RUN            = int("number-of-this-run")
 
-        STATIONS                      = inputFile("stations", default="DATA/STATIONS")
+        stations                      = inputFile("stations", default="DATA/STATIONS")
 
     def __init__(self, name):
         Component.__init__(self, name, "solver")
+        self.CMTSOLUTION = None
+        self.STATIONS = None
 
     def _init(self):
         Component._init(self)
@@ -39,11 +41,10 @@ class Solver(Component):
         # convert to minutes
         self.RECORD_LENGTH_IN_MINUTES = self.inventory.record_length / minute
 
-        # Access our InputFile inventory items, forcing them to be
-        # opened, to make sure they're readable.  Then close them
-        # (they will be reopened by the Fortran code).
-        self.inventory.CMTSOLUTION.close()
-        self.inventory.STATIONS.close()
+        # Access our InputFile inventory items to make sure they're
+        # readable.  (They will be reopened by the Fortran code.)
+        f = self.inventory.cmtSolution;  self.CMTSOLUTION = f.name;  f.close()
+        f = self.inventory.stations;     self.STATIONS    = f.name;  f.close()
 
 
 # end of file
