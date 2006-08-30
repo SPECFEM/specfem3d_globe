8e6b4cb6ac46bef76066d2ff97dc3072aa275736
Leif Strand <leif@geodynamics.org>
2006-08-30 20:56:19 +0000

    Gave the daemon the ability to post "multipart/form-data", so that it can upload the simulation output to the web server.

--- Specfem3DGlobe/web/Daemon.py
+++ Specfem3DGlobe/web/Daemon.py
@@ -11,6 +11,7 @@ simulationsUrl      = urlPrefix + 'simulations/list.py'
 inputFileUrl        = urlPrefix + 'simulations/%d/%s'
 inputFiles          = ['parameters.pml', 'events.txt', 'stations.txt']
 simStatusUrl        = urlRoot + 'simulations/%s/status/'
+multipartBoundary   = '----------eArThQuAkE$'
 
 #
 # destination 
@@ -30,14 +31,52 @@ class SimStatus(object):
     def poll(self, sim): pass
     
     def postStatusChange(self, sim, newStatus):
-        import httplib
         import urllib
         id = sim['id']
-        params = urllib.urlencode({'status': newStatus.__name__})
-        headers = {"Content-type": "application/x-www-form-urlencoded",
+        body = urllib.urlencode({'status': newStatus.__name__})
+        headers = {"Content-Type": "application/x-www-form-urlencoded",
                    "Accept": "text/plain"}
+        self.post(id, body, headers)
+        return
+
+    def postStatusChangeAndUploadOutput(self, sim, newStatus, output):
+        # Based upon a recipe by Wade Leftwich.
+        id = sim['id']
+        fields = {'status': newStatus.__name__}
+        files = [('output', 'output.txt', 'application/x-gtar', 'gzip', output)]
+        body = self.encodeMultipartFormData(fields, files)
+        headers = {"Content-Type": "multipart/form-data; boundary=%s" % multipartBoundary,
+                   "Content-Length": str(len(body)),
+                   "Accept": "text/plain"}
+        self.post(id, body, headers)
+        return
+
+    def encodeMultipartFormData(self, fields, files):
+        import shutil
+        from StringIO import StringIO
+        stream = StringIO()
+        def line(s=''): stream.write(s + '\r\n')
+        for key, value in fields.iteritems():
+            line('--' + multipartBoundary)
+            line('Content-Disposition: form-data; name="%s"' % key)
+            line()
+            line(value)
+        for (key, filename, contentType, contentEncoding, content) in files:
+            line('--' + multipartBoundary)
+            line('Content-Disposition: form-data; name="%s"; filename="%s"' % (key, filename))
+            line('Content-Type: %s' % contentType)
+            line('Content-Encoding: %s' % contentEncoding)
+            line()
+            shutil.copyfileobj(content, stream)
+            line()
+        line('--' + multipartBoundary + '--')
+        line()
+        return stream.getvalue()
+
+    def post(self, id, body, headers):
+        import httplib
         conn = httplib.HTTPConnection(host)
-        conn.request("POST", (simStatusUrl % id), params, headers)
+        conn.request("POST", (simStatusUrl % id), body, headers)
         response = conn.getresponse()
         data = response.read()
         conn.close()
@@ -55,7 +94,7 @@ class SimStatusNew(SimStatus):
         id = sim['id']
         for inputFile in inputFiles:
             infile = urllib2.urlopen(inputFileUrl % (id, inputFile))
-            outfile = open('%s/%d-%s' % (sSaveDir, id, inputFile), 'w')
+            outfile = open('%s/%d-%s' % (sSaveDir, id, inputFile), 'wb')
             shutil.copyfileobj(infile,  outfile)
             outfile.close()
             infile.close()
@@ -82,7 +121,9 @@ class SimStatusRunning(SimStatus):
     def poll(self, sim):
         id = sim['id']
         print "check job status (running)", id
-        self.postStatusChange(sim, SimStatusDone)
+        output = open('/home/leif/monolith/pylith-0.8.1a-linux-x86.tar.gz', 'rb')
+        self.postStatusChangeAndUploadOutput(sim, SimStatusDone, output)
+        output.close()
         return
 
 
