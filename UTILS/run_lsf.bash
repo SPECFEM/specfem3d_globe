fb5240f7bda10a4778265776cfe5db247c719a25
Qinya Liu <liuqy@physics.utoronto.ca>
2006-12-30 19:53:14 +0000

    fix bug 'cut'

--- UTILS/run_lsf.bash
+++ UTILS/run_lsf.bash
@@ -20,7 +20,7 @@ echo "Finished compilation $d"
 # compute total number of nodes needed
 NPROC_XI=`grep NPROC_XI DATA/Par_file | cut -d = -f 2 `
 NPROC_ETA=`grep NPROC_ETA DATA/Par_file | cut -d = -f 2`
-NCHUNKS=`grep NCHUNKS DATA/Par_file | cut cut -d = -f 2`
+NCHUNKS=`grep NCHUNKS DATA/Par_file | cut -d = -f 2`
 
 
 # total number of nodes is the product of the values read
