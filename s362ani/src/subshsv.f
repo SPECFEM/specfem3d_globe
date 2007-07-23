d80bf3df59c7a89a9cbf842936627bf7b145f1c7
Jeroen Tromp <jtromp@princeton.edu>
2007-07-23 14:42:23 +0000

    I have tested the new 1D model REF, which is working fine. The 3D Harvard models S362ANI, S362WMANI, S362ANI_PREM and S2.9EA have also been incorporated. I will continue to test these, including the topographic variations on the 410 km and 650 km discontinuities. I needed to add the new REF models to the write_AVS_DX_global_chunks_data.f90 routine.

--- s362ani/src/subshsv.f
+++ s362ani/src/subshsv.f
@@ -1,19 +1,11 @@
 c --- evaluate perturbations in per cent 
 
-	subroutine subshsv(xcolat,xlon,xrad,
-     #         vshout,vsvout,vphout,vpvout,etaout,rhoout,ifknowmodel,
-     #         THREE_D_MODEL,THREE_D_MODEL_S362ANI,
-     #         THREE_D_MODEL_S362WMANI,
-     #         THREE_D_MODEL_S362ANI_PREM,THREE_D_MODEL_S29EA)
+	subroutine subshsv(xcolat,xlon,xrad,dvsh,dvsv,dvph,dvpv)
 
 	implicit none
 
 	real*4 xcolat,xlon,xrad
-	real*4 vshout,vsvout,vphout,vpvout,etaout,rhoout
-	integer ifknowmodel
-	integer THREE_D_MODEL,THREE_D_MODEL_S362ANI
-	integer THREE_D_MODEL_S362WMANI
-	integer THREE_D_MODEL_S362ANI_PREM,THREE_D_MODEL_S29EA
+	real*4 dvsh,dvsv,dvph,dvpv
  
 c --- model evaluation
 
@@ -29,64 +21,14 @@ c --- model evaluation
 
 c --- 
 	
-	character*128 modeldef
 	integer iker,i
 	character*40 vstr
-	logical exists
-	integer lu,numvar,lstr
+	integer lstr
 	integer lnblnk,ierror
 
 	include 'mod.h'
 
 c -------------------------------------
-
-	lu=1 			! --- log unit: input 3-D model 
-
-c --- read the model if necessary
-
-	if(ifknowmodel.ne.0) then
-	else
-          if(THREE_D_MODEL .eq. THREE_D_MODEL_S362ANI) then
-	    modeldef='DATA/s362ani/S362ANI'
-          elseif(THREE_D_MODEL .eq. THREE_D_MODEL_S362WMANI) then
-	    modeldef='DATA/s362ani/S362WMANI'
-          elseif(THREE_D_MODEL .eq. THREE_D_MODEL_S362ANI_PREM) then
-	    modeldef='DATA/s362ani/S362ANI_PREM'
-          elseif(THREE_D_MODEL .eq. THREE_D_MODEL_S29EA) then
-	    modeldef='DATA/s362ani/S2.9EA'
-          else
-            stop 'unknown 3D model in subshsv'
-          endif
-          inquire(file=modeldef,exist=exists)
-          if(exists) then
-            call gt3dmodl(lu,modeldef,
-     #        maxhpa,maxker,maxcoe,
-     #        numhpa,numker,numcoe,lmxhpa,
-     #        ihpakern,itypehpa,coe,
-     #        itpspl,xlaspl,xlospl,radspl,
-     #        numvar,ivarkern,varstr,
-     #        refmdl,kerstr,hsplfl,dskker,ierror) 
-	  else
-	    write(6,"('the model ',a,' does not exits')")
-     #             modeldef(1:lnblnk(modeldef))
-	  endif
-	  ifknowmodel=1
-
-c 	  --- check arrays
-
-	  if(numker.gt.maxker) stop 'numker.gt.maxker'
-          do ihpa=1,numhpa
-           if(itypehpa(ihpa).eq.1) then
-	    if(lmxhpa(ihpa).gt.maxl) stop 'lmxhpa(ihpa).gt.maxl'
-	   else if(itypehpa(ihpa).eq.2) then
- 	    if(numcoe(ihpa).gt.maxcoe) stop 'numcoe(ihpa).gt.maxcoe'
-	   else 
-	    stop 'problem with itypehpa'
-	   endif
-	  enddo
-	endif
-
-c --- evaluate radial basis functions
 
 	depth=6371.0-xrad
 	call evradker (depth,kerstr,numker,vercof,vercofd,ierror)
@@ -180,12 +122,10 @@ c 	  --- evaluate perturbations in vsh and vsv
 
 c --- evaluate perturbations in per cent
 
-	vshout=100.0*vsh3drel
-	vsvout=100.0*vsv3drel
-	vphout=0.55*vshout	! --- scaling used in the inversion
-	vpvout=0.55*vsvout	! --- scaling used in the inversion
-	etaout=0.0		! --- not inverted for
-	rhoout=0.0		! --- not inverted for
+	dvsh=vsh3drel
+	dvsv=vsv3drel
+	dvph=0.55*dvsh	! --- scaling used in the inversion
+	dvpv=0.55*dvsv	! --- scaling used in the inversion
 	end
 
 
--- s362ani/src/subtopo.f
+++ s362ani/src/subtopo.f
@@ -1,18 +1,11 @@
 c --- evaluate depressions of the 410- and 650-km discontinuities in km
 
-	subroutine subtopo(xcolat,xlon,topo410out,topo650out,ifknowmodel,
-     #         THREE_D_MODEL,THREE_D_MODEL_S362ANI,
-     #         THREE_D_MODEL_S362WMANI,
-     #         THREE_D_MODEL_S362ANI_PREM,THREE_D_MODEL_S29EA)
+	subroutine subtopo(xcolat,xlon,topo410,topo650)
 
 	implicit none
 
 	real*4 xcolat,xlon
-	real*4 topo410out,topo650out
-	integer ifknowmodel
-	integer THREE_D_MODEL,THREE_D_MODEL_S362ANI
-	integer THREE_D_MODEL_S362WMANI
-	integer THREE_D_MODEL_S362ANI_PREM,THREE_D_MODEL_S29EA
+	real*4 topo410,topo650
 
 c --- model evaluation
 
@@ -23,62 +16,14 @@ c --- model evaluation
 	real*4 x,y	! --- lat lon
 
 c --- 
-	character*128 modeldef
 	integer iker,i
 	character*40 vstr
-	logical exists
-	integer lu,numvar,lstr
-	integer lnblnk,ierror
+	integer lstr
+	integer lnblnk
 
 	include 'mod.h'
 
 c -------------------------------------
-
-	lu=1 			! --- log unit: input 3-D model 
-
-c --- read the model if necessary
-
-	if(ifknowmodel.ne.0) then
-	else
-	  if(THREE_D_MODEL .eq. THREE_D_MODEL_S362ANI) then
-            modeldef='DATA/s362ani/S362ANI'
-          elseif(THREE_D_MODEL .eq. THREE_D_MODEL_S362WMANI) then
-            modeldef='DATA/s362ani/S362WMANI'
-          elseif(THREE_D_MODEL .eq. THREE_D_MODEL_S362ANI_PREM) then
-            modeldef='DATA/s362ani/S362ANI_PREM'
-          elseif(THREE_D_MODEL .eq. THREE_D_MODEL_S29EA) then
-            modeldef='DATA/s362ani/S2.9EA'
-          else
-            stop 'unknown 3D model in subshsv'
-          endif
-          inquire(file=modeldef,exist=exists)
-          if(exists) then
-            call gt3dmodl(lu,modeldef,
-     #        maxhpa,maxker,maxcoe,
-     #        numhpa,numker,numcoe,lmxhpa,
-     #        ihpakern,itypehpa,coe,
-     #        itpspl,xlaspl,xlospl,radspl,
-     #        numvar,ivarkern,varstr,
-     #        refmdl,kerstr,hsplfl,dskker,ierror) 
-	  else
-	    write(6,"('the model ',a,' does not exits')")
-     #              modeldef(1:lnblnk(modeldef))
-	  endif
-	  ifknowmodel=1
-
-c 	  --- check arrays
-
-	  if(numker.gt.maxker) stop 'numker.gt.maxker'
-          do ihpa=1,numhpa
-           if(itypehpa(ihpa).eq.1) then
-	    if(lmxhpa(ihpa).gt.maxl) stop 'lmxhpa(ihpa).gt.maxl'
-	   else if(itypehpa(ihpa).eq.2) then
- 	    if(numcoe(ihpa).gt.maxcoe) stop 'numcoe(ihpa).gt.maxcoe'
-	   else 
-	    stop 'problem with itypehpa'
-	   endif
-	  enddo
-	endif		
 
 c       --- contributing horizontal basis functions at xlat,xlon
 	  
@@ -142,8 +87,8 @@ c         --- evaluate topography (depression) in km
 	    valu(ieval)=value
 	enddo ! --- ieval
 
-	topo410out=valu(1) 	
-	topo650out=valu(2) 	
+	topo410=valu(1) 	
+	topo650=valu(2) 	
 	end
 
 
