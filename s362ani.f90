8a1f17b203fa5e3701001be7e73d52ded6697300
Dimitri Komatitsch <komatitsch@lma.cnrs-mrs.fr>
2007-08-13 21:00:56 +0000

    suppressed unused getcurrmodels subroutine, which produced compilation errors with IBM  xlf

--- s362ani.f90
+++ s362ani.f90
@@ -583,38 +583,6 @@
   end subroutine chebyfun
 
 
-  subroutine getcurrmodels(model,imod)
-
-  implicit none
-
-  character(len=*) model
-  integer :: imod
-  logical exists
-
-  integer :: ios,i
-
-  inquire(file='current_3d_model',exist=exists)
-  model=''
-  if(exists) then
-  open(13,file='current_3d_model')
-  ios=0
-  do i=1,imod
-  if(ios == 0) read(13,"(a)",iostat=ios) model
-  enddo
-  close(13)
-  if(ios == 0) then
-  inquire(file=model,exist=exists)
-  if(exists) then
-  else
-    model=''
-    endif
-  else
-  model=''
-  endif
-  endif
-
-  end subroutine getcurrmodels
-
   subroutine gt3dmodl(lu,targetfile, &
       maxhpa,maxker,maxcoe, &
       numhpa,numker,numcoe,lmxhpa, &
