a7fe66e904980f9cdb2b2c6913c87db10e3d73ef
Dimitri Komatitsch <komatitsch@lma.cnrs-mrs.fr>
2007-08-13 21:23:23 +0000

    also suppressed unused putcurrmodels subroutine, which produced a compilation error with IBM xlf

--- s362ani.f90
+++ s362ani.f90
@@ -710,33 +710,6 @@
   end subroutine gt3dmodl
 
 
-  subroutine putcurrmodels(model,imod)
-
-  implicit none
-
-  integer :: imod
-
-  character(len=*) model
-  character(len=80) string(10)
-
-  integer :: i,ios
-
-  open(13,file='current_3d_model')
-  ios=0
-  do i=1,10
-  string(i)=''
-  if(ios == 0) read(13,"(a)",iostat=ios) string(i)
-  enddo
-  string(imod)=model
-  rewind(13)
-  do i=1,10
-  write(13,"(a)") string(i)
-  enddo
-  close(13)
-
-  end subroutine putcurrmodels
-
-
   subroutine rd3dmodl(lu,filename,ierror, &
     nmodkern,nhorpar,ityphpar, &
     ihorpar,lmaxhor,ncoefhor, &
