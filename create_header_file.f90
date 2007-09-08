c7237fef45abdfe5cadc2a66e94f273bc6458a18
Dimitri Komatitsch <komatitsch@lma.cnrs-mrs.fr>
2007-09-08 13:07:02 +0000

    changed 1.6 GB to 1.7 GB in memory estimate for MareNostrum

--- create_header_file.f90
+++ create_header_file.f90
@@ -187,7 +187,7 @@
   print *,'size of static arrays per slice = ',static_memory_size/1073741824.d0,' GB'
   print *
   print *,'   (should be below and typically equal to 80% of 1.5 GB = 1.2 GB on pangu'
-  print *,'    at Caltech, and below and typically equal to 80% of 2 GB = 1.6 GB'
+  print *,'    at Caltech, and below and typically equal to 85% of 2 GB = 1.7 GB'
   print *,'    on Marenostrum in Barcelona)'
   print *,'   (if significantly more, the job will not run by lack of memory)'
   print *,'   (if significantly less, you waste a significant amount of memory)'
