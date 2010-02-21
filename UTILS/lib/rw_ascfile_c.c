652ad1402fb4a1670e631d5c1b895b61e3779a1a
Daniel Peter <peterda@ethz.ch>
2010-02-21 01:38:56 +0000

    adds in directory UTILS/cut_velocity a Makefile to use `make` for compilation rather than the provided script compile_cut

--- UTILS/lib/rw_ascfile_c.c
+++ UTILS/lib/rw_ascfile_c.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-#include "drw_ascfile.h"
+#include <stdlib.h>
+//#include "drw_ascfile.h"
+// Qinya Liu, Caltech, May 2007
 
-void dread_ascfile(const char *ascfile,
+
+void dread_ascfile_c_(const char *ascfile,
 		   double *t0, double *dt, int *n,
 		   double *data) 
 
@@ -18,33 +21,41 @@ void dread_ascfile(const char *ascfile,
   while ( fscanf(fd,"%lf  %lf\n",&junk, data+i) != EOF ) {
     if (i == 0) junk1 = junk;
     if (i == 1) *dt = junk - junk1;
-    i++;}
+    i++;
+  }
   *t0 = junk1;
   *n = i;
   if (fclose(fd) != 0) {
     printf(" file %s cannot be closed\n",ascfile);
-    exit(1);}
+    exit(1);
+  }
 
 }
 
-void dwrite_ascfile(const char *ascfile,
-		    double t0, double dt, int n,
+void dwrite_ascfile_c_(const char *ascfile,
+		    double *t0, double *dt, int *n,
 		    const double *data) 
 
 {
   FILE *fd;
   int i;
+  double tmp,tmp0;
+  
+  //printf("t0: %f ,dt: %f ,n: %i \n",*t0,*dt,*n);
   
   if ((fd = fopen(ascfile,"w")) == NULL) {
     printf(" file %s cannot be opened to write\n",ascfile);
     exit(1);
   }
   i = 0;
-  for (i=0; i<n; i++) {
-    fprintf(fd,"%14.7g %18.7g\n", t0+i*dt, data[i]);
+  tmp = *dt;
+  tmp0 = *t0;
+  for (i=0; i< *n; i++) {  
+    fprintf(fd,"%14.7g %18.7g\n", tmp0+i*tmp, data[i]);
   }
   if (fclose(fd) != 0) {
     printf("file %s cannot be closed\n",ascfile);
-    exit(1);}
+    exit(1);
+  }
 
 }
