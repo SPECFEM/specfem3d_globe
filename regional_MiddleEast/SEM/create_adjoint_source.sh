564a02888ce82cbaf458cf139821e49c0b2cbe71
Daniel Peter <peterda@ethz.ch>
2011-03-02 03:24:08 +0000

    moves source files in SPECFEM3D_GLOBE/ into corresponding subdirectories for mesher and solver; updates Makefiles and configure

---
+++ regional_MiddleEast/SEM/create_adjoint_source.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# creates adjoint source files
+#
+# note: run this script in directory SEM/
+#############################################################
+
+# station
+station="GAR"
+network="II"
+
+# time window
+t1="190."
+t2="225."
+
+#window out single phase arrival on vertical component between t1 to t2 :
+~/SPECFEM3D_GLOBE/UTILS/cut_velocity/xcut_velocity $t1 $t2 3 ../REF_SEIS/$station.$network.MX*
+mv ../REF_SEIS/$station.$network.MX*adj ./
+
+# rename adjoint source files:
+rename .sem.ascii.adj .adj $station.$network.MX*adj
+
+# create STATIONS_ADJOINT file with adjoint source location
+fgrep $station ../DATA/STATIONS > ./STATIONS_ADJOINT
+
