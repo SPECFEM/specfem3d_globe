96724f51a9de5db2d45e8a61b632b8fdcb1dbe6c
Daniel Peter <peterda@ethz.ch>
2011-04-20 22:51:03 +0000

    customized shakemovie version v5.1.1-shakemovie to run synthetics for website http://global.shakemovie.princeton.edu

--- doc/USER_MANUAL/create_icon_image_for_CIG_Web_site.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# Dimitri Komatitsch, November 2010: use "convert" from the Imagemagick package
-
-convert -quality 100 -resize 38% -colorspace RGB figures/specfem_3d_globe-cover.pdf jpg:specfem_3d_globe-cover_small.jpg
-
