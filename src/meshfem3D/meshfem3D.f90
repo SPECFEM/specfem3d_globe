97d8e610605abe62db1cdf44e183b9a57c10cc36
Dimitri Komatitsch <komatitsch@lma.cnrs-mrs.fr>
2012-05-19 14:09:44 +0000

    fixed the USE_COMPONENT bug of create_movie_AVS_DX()

--- src/meshfem3D/meshfem3D.f90
+++ src/meshfem3D/meshfem3D.f90
@@ -216,7 +216,7 @@
 !      added AK135 and 1066a, fixed topography/bathymetry routine,
 !      new attenuation routines, faster and better I/Os on very large
 !      systems, many small improvements and bug fixes, new "configure"
-!      script, new Pyre version, new user's manual etc.
+!      script, new user's manual etc.
 !
 ! v. 3.5 Dimitri Komatitsch, Brian Savage and Jeroen Tromp, Caltech, July 2004:
 !      any size of chunk, 3D attenuation, case of two chunks,
