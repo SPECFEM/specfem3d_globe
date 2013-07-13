5b8d21e8c7aaabfeb765d5943809b864872ce8de
Dimitri Komatitsch <komatitsch@lma.cnrs-mrs.fr>
2013-07-13 22:47:26 +0000

    got rid of "if(COMPUTE_AND_STORE_STRAIN) then" test in compute_kernels() because COMPUTE_AND_STORE_STRAIN is always set to true for kernel calculations i.e. when SIMULATION_TYPE /= 1

--- src/specfem3D/part3_kernel_computation.f90
+++ src/specfem3D/part3_kernel_computation.f90
@@ -8,7 +8,7 @@
                           rho_kl_crust_mantle,beta_kl_crust_mantle, &
                           alpha_kl_crust_mantle,cijkl_kl_crust_mantle, &
                           accel_crust_mantle,b_displ_crust_mantle, &
-                          deltat,displ_crust_mantle,hprime_xx,hprime_xxT,&
+                          deltat,hprime_xx,hprime_xxT,&
                           xix_crust_mantle,xiy_crust_mantle,xiz_crust_mantle,&
                           etax_crust_mantle,etay_crust_mantle,etaz_crust_mantle,&
                           gammax_crust_mantle,gammay_crust_mantle,gammaz_crust_mantle,ANISOTROPIC_KL,&
@@ -34,7 +34,7 @@
                           rho_kl_inner_core,beta_kl_inner_core, &
                           alpha_kl_inner_core, &
                           accel_inner_core,b_displ_inner_core, &
-                          deltat,displ_inner_core,hprime_xx,hprime_xxT,&
+                          deltat,hprime_xx,hprime_xxT,&
                           xix_inner_core,xiy_inner_core,xiz_inner_core,&
                           etax_inner_core,etay_inner_core,etaz_inner_core,&
                           gammax_inner_core,gammay_inner_core,gammaz_inner_core,&
