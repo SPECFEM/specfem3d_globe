5304f2eb58cc50bec52649dc8cd332dd98ef5488
Dimitri Komatitsch <see_my_GitHub_web_page>
2014-05-09 00:22:07 +0200

    added topo_bathy/topo_bathy_etopo2_original_unmodified_unsmoothed.dat and topo_bathy/topo_bathy_etopo2_smoothed_window_3.dat and updated topo_bathy/smooth_topo_bathy_PPM_image.f90

--- topo_bathy/smooth_topo_bathy_PPM_image.f90
+++ topo_bathy/smooth_topo_bathy_PPM_image.f90
@@ -38,11 +38,11 @@
 
   implicit none
 
-  include "../../constants.h"
+  include "../../setup/constants.h"
 
 ! filter final surface using box filter
   logical, parameter :: SMOOTH_THE_MODEL = .true.
-  integer, parameter :: SIZE_FILTER_ONE_SIDE = 7
+  integer, parameter :: SIZE_FILTER_ONE_SIDE = 3 !! 7
 
 ! use integer array to store values
   integer ibathy_topo(NX_BATHY,NY_BATHY)
@@ -62,7 +62,7 @@
   print *
   print *,'reading topo file'
 
-  open(unit=13,file='topo_bathy_etopo4_from_etopo2_subsampled.dat',status='old')
+  open(unit=13,file='topo_bathy_etopo2_original_unmodified_unsmoothed.dat',status='old')
   do iy=1,NY_BATHY
     do ix=1,NX_BATHY
       read(13,*) ibathy_topo_ori(ix,iy)
@@ -148,7 +148,7 @@
   if(SMOOTH_THE_MODEL) then
     print *
     print *,'saving the smoothed model'
-    open(unit=13,file='topo_bathy_etopo4_smoothed_window_7.dat',status='unknown')
+    open(unit=13,file='topo_bathy_etopo2_smoothed_window_3.dat',status='unknown')
     do iy=1,NY_BATHY
       do ix=1,NX_BATHY
         write(13,*) ibathy_topo(ix,iy)
