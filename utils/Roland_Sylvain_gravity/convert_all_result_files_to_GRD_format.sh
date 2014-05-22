1cb1024aea1eeb45b8b583e9cdb8f792c13d23a8
Dimitri Komatitsch <komatitsch@lma.cnrs-mrs.fr>
2014-05-23 01:43:53 +0200

    added "blockmedian" to GMT script; added output of time stamp file for ispec == 1

--- utils/Roland_Sylvain_gravity/convert_all_result_files_to_GRD_format.sh
+++ utils/Roland_Sylvain_gravity/convert_all_result_files_to_GRD_format.sh
@@ -1,16 +1,35 @@
 #!/bin/bash
 
- surface results_g_x_for_GMT.txt -Gresults_g_x.grd -Rd -I4m -f0x,1y -V
- surface results_g_y_for_GMT.txt -Gresults_g_y.grd -Rd -I4m -f0x,1y -V
- surface results_g_z_for_GMT.txt -Gresults_g_z.grd -Rd -I4m -f0x,1y -V
- surface results_norm_of_g_for_GMT.txt -Gresults_norm_of_g.grd -Rd -I4m -f0x,1y -V
- surface results_G_xx_for_GMT.txt -Gresults_G_xx.grd -Rd -I4m -f0x,1y -V
- surface results_G_yy_for_GMT.txt -Gresults_G_yy.grd -Rd -I4m -f0x,1y -V
- surface results_G_zz_for_GMT.txt -Gresults_G_zz.grd -Rd -I4m -f0x,1y -V
- surface results_G_xy_for_GMT.txt -Gresults_G_xy.grd -Rd -I4m -f0x,1y -V
- surface results_G_xz_for_GMT.txt -Gresults_G_xz.grd -Rd -I4m -f0x,1y -V
- surface results_G_yz_for_GMT.txt -Gresults_G_yz.grd -Rd -I4m -f0x,1y -V
+ blockmedian -Rd -I4m -V results_g_x_for_GMT.txt > ________temp________.xyz
+ surface ________temp________.xyz -Gresults_g_x.grd -Rd -I4m -f0x,1y -V
+
+ blockmedian -Rd -I4m -V results_g_y_for_GMT.txt > ________temp________.xyz
+ surface ________temp________.xyz -Gresults_g_y.grd -Rd -I4m -f0x,1y -V
+
+ blockmedian -Rd -I4m -V results_g_z_for_GMT.txt > ________temp________.xyz
+ surface ________temp________.xyz -Gresults_g_z.grd -Rd -I4m -f0x,1y -V
+
+ blockmedian -Rd -I4m -V results_norm_of_g_for_GMT.txt > ________temp________.xyz
+ surface ________temp________.xyz -Gresults_norm_of_g.grd -Rd -I4m -f0x,1y -V
+
+ blockmedian -Rd -I4m -V results_G_xx_for_GMT.txt > ________temp________.xyz
+ surface ________temp________.xyz -Gresults_G_xx.grd -Rd -I4m -f0x,1y -V
+
+ blockmedian -Rd -I4m -V results_G_yy_for_GMT.txt > ________temp________.xyz
+ surface ________temp________.xyz -Gresults_G_yy.grd -Rd -I4m -f0x,1y -V
+
+ blockmedian -Rd -I4m -V results_G_zz_for_GMT.txt > ________temp________.xyz
+ surface ________temp________.xyz -Gresults_G_zz.grd -Rd -I4m -f0x,1y -V
+
+ blockmedian -Rd -I4m -V results_G_xy_for_GMT.txt > ________temp________.xyz
+ surface ________temp________.xyz -Gresults_G_xy.grd -Rd -I4m -f0x,1y -V
+
+ blockmedian -Rd -I4m -V results_G_xy_for_GMT.txt > ________temp________.xyz
+ surface ________temp________.xyz -Gresults_G_xz.grd -Rd -I4m -f0x,1y -V
+
+ blockmedian -Rd -I4m -V results_G_yz_for_GMT.txt > ________temp________.xyz
+ surface ________temp________.xyz -Gresults_G_yz.grd -Rd -I4m -f0x,1y -V
 
 # Clean up
-rm -f .gmt*
+rm -f .gmt* ________temp________.xyz
 
