98758145dce0909ddb723659d96d6a5cf3f3e494
Elliott Sales de Andrade <esalesde@physics.utoronto.ca>
2014-09-11 20:19:41 -0400

    Remove memory_eval from specfem3D.

--- src/specfem3D/rules.mk
+++ src/specfem3D/rules.mk
@@ -137,7 +137,6 @@ specfem3D_SHARED_OBJECTS = \
 	$O/intgrl.shared.o \
 	$O/lagrange_poly.shared.o \
 	$O/make_ellipticity.shared.o \
-	$O/memory_eval.shared.o \
 	$O/model_prem.shared.o \
 	$O/model_topo_bathy.shared.o \
 	$O/parallel.sharedmpi.o \
