4cf4cfc0a0597bec6fadf52cbd2defe5fec35588
Dimitri Komatitsch <komatitsch@lma.cnrs-mrs.fr>
2014-09-23 12:23:45 +0200

    allow the code to use up to 100% of the memory instead of 92% when using UNDO_ATTENUATION on GPUs because UNDO_ATTENUATION buffers are then stored on another machine (the host) instead of on the GPU itself

--- src/shared/read_compute_parameters.f90
+++ src/shared/read_compute_parameters.f90
@@ -382,13 +382,18 @@
 
   if (MEMORY_INSTALLED_PER_CORE_IN_GB < 0.1d0) &
        stop 'less than 100 MB per core for MEMORY_INSTALLED_PER_CORE_IN_GB does not seem realistic; exiting...'
-  if (MEMORY_INSTALLED_PER_CORE_IN_GB > 200.d0) &
-       stop 'more than 200 GB per core for MEMORY_INSTALLED_PER_CORE_IN_GB does not seem realistic; exiting...'
+!! DK DK the value below will probably need to be increased one day, on future machines
+  if (MEMORY_INSTALLED_PER_CORE_IN_GB > 512.d0) &
+       stop 'more than 512 GB per core for MEMORY_INSTALLED_PER_CORE_IN_GB does not seem realistic; exiting...'
 
   if (PERCENT_OF_MEM_TO_USE_PER_CORE < 50.d0) &
        stop 'less than 50% for PERCENT_OF_MEM_TO_USE_PER_CORE does not seem realistic; exiting...'
-  if (PERCENT_OF_MEM_TO_USE_PER_CORE > 92.d0) &
-       stop 'more than 92% for PERCENT_OF_MEM_TO_USE_PER_CORE is risky; exiting...'
+  if (PERCENT_OF_MEM_TO_USE_PER_CORE > 100.d0) &
+       stop 'more than 100% for PERCENT_OF_MEM_TO_USE_PER_CORE makes no sense; exiting...'
+!! DK DK will need to remove the ".and. .not. GPU_MODE" test here
+!! DK DK if the undo_attenuation buffers are stored on the GPU instead of on the host
+  if (PERCENT_OF_MEM_TO_USE_PER_CORE > 92.d0 .and. .not. GPU_MODE) &
+       stop 'more than 92% for PERCENT_OF_MEM_TO_USE_PER_CORE when not using GPUs is risky; exiting...'
 
   what_we_can_use_in_GB = MEMORY_INSTALLED_PER_CORE_IN_GB * PERCENT_OF_MEM_TO_USE_PER_CORE / 100.d0
 
