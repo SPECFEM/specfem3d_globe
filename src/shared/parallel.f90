f957c11ca72e0d35df522f0984ca855b6e74176d
Matthieu Lefebvre <ml15@princeton.edu>
2014-12-01 14:43:38 -0500

    Copied comm splitters from cartesian. updates parameters

--- src/shared/parallel.f90
+++ src/shared/parallel.f90
@@ -25,6 +25,41 @@
 !
 !=====================================================================
 
+!! DK DK July 2014, CNRS Marseille, France:
+!! DK DK added the ability to run several calculations (several earthquakes)
+!! DK DK in an embarrassingly-parallel fashion from within the same run;
+!! DK DK this can be useful when using a very large supercomputer to compute
+!! DK DK many earthquakes in a catalog, in which case it can be better from
+!! DK DK a batch job submission point of view to start fewer and much larger jobs,
+!! DK DK each of them computing several earthquakes in parallel.
+!! DK DK To turn that option on, set parameter NUMBER_OF_SIMULTANEOUS_RUNS
+!! DK DK to a value greater than 1 in file setup/constants.h.in before
+!! DK DK configuring and compiling the code.
+!! DK DK To implement that, we create NUMBER_OF_SIMULTANEOUS_RUNS MPI sub-communicators,
+!! DK DK each of them being labeled "my_local_mpi_comm_world", and we use them
+!! DK DK in all the routines in "src/shared/parallel.f90", except in MPI_ABORT() because in that case
+!! DK DK we need to kill the entire run.
+!! DK DK When that option is on, of course the number of processor cores used to start
+!! DK DK the code in the batch system must be a multiple of NUMBER_OF_SIMULTANEOUS_RUNS,
+!! DK DK all the individual runs must use the same number of processor cores,
+!! DK DK which as usual is NPROC in the input file DATA/Par_file,
+!! DK DK and thus the total number of processor cores to request from the batch system
+!! DK DK should be NUMBER_OF_SIMULTANEOUS_RUNS * NPROC.
+!! DK DK All the runs to perform must be placed in directories called run0001, run0002, run0003 and so on
+!! DK DK (with exactly four digits).
+
+module my_mpi
+
+! main parameter module for specfem simulations
+
+  use mpi
+
+  implicit none
+
+  integer :: my_local_mpi_comm_world, my_local_mpi_comm_for_bcast
+
+end module my_mpi
+
 !-------------------------------------------------------------------------------------------------
 !
 ! MPI wrapper functions
@@ -1319,4 +1354,111 @@
   info = MPI_INFO_NULL
 
   end subroutine world_get_info_null
+
+!
+!-------------------------------------------------------------------------------------------------
+!
+
+! create sub-communicators if needed, if running more than one earthquake from the same job.
+!! DK DK create a sub-communicator for each independent run;
+!! DK DK if there is a single run to do, then just copy the default communicator to the new one
+  subroutine world_split()
+
+  use my_mpi
+  use constants,only: MAX_STRING_LEN,NUMBER_OF_SIMULTANEOUS_RUNS,OUTPUT_FILES_PATH, &
+    IMAIN,ISTANDARD_OUTPUT,mygroup,BROADCAST_SAME_MESH_AND_MODEL,I_should_read_the_database
+
+  implicit none
+
+  integer :: sizeval,myrank,ier,key,my_group_for_bcast,my_local_rank_for_bcast,NPROC
+
+  character(len=MAX_STRING_LEN) :: path_to_add
+
+  if (NUMBER_OF_SIMULTANEOUS_RUNS <= 0) stop 'NUMBER_OF_SIMULTANEOUS_RUNS <= 0 makes no sense'
+
+  call MPI_COMM_SIZE(MPI_COMM_WORLD,sizeval,ier)
+  call MPI_COMM_RANK(MPI_COMM_WORLD,myrank,ier)
+
+  if (NUMBER_OF_SIMULTANEOUS_RUNS > 1 .and. mod(sizeval,NUMBER_OF_SIMULTANEOUS_RUNS) /= 0) &
+    stop 'the number of MPI processes is not a multiple of NUMBER_OF_SIMULTANEOUS_RUNS'
+
+  if (NUMBER_OF_SIMULTANEOUS_RUNS > 1 .and. IMAIN == ISTANDARD_OUTPUT) &
+    stop 'must not have IMAIN == ISTANDARD_OUTPUT when NUMBER_OF_SIMULTANEOUS_RUNS > 1 otherwise output to screen is mingled'
+
+  if (NUMBER_OF_SIMULTANEOUS_RUNS == 1) then
+
+    my_local_mpi_comm_world = MPI_COMM_WORLD
+
+! no broadcast of the mesh and model databases to other runs in that case
+    my_group_for_bcast = 0
+    my_local_mpi_comm_for_bcast = MPI_COMM_NULL
+
+  else
+
+!--- create a subcommunicator for each independent run
+
+    NPROC = sizeval / NUMBER_OF_SIMULTANEOUS_RUNS
+
+!   create the different groups of processes, one for each independent run
+    mygroup = myrank / NPROC
+    key = myrank
+    if (mygroup < 0 .or. mygroup > NUMBER_OF_SIMULTANEOUS_RUNS-1) stop 'invalid value of mygroup'
+
+!   build the sub-communicators
+    call MPI_COMM_SPLIT(MPI_COMM_WORLD, mygroup, key, my_local_mpi_comm_world, ier)
+    if (ier /= 0) stop 'error while trying to create the sub-communicators'
+
+!   add the right directory for that run (group numbers start at zero, but directory names start at run0001, thus we add one)
+    write(path_to_add,"('run',i4.4,'/')") mygroup + 1
+    OUTPUT_FILES_PATH = path_to_add(1:len_trim(path_to_add))//OUTPUT_FILES_PATH(1:len_trim(OUTPUT_FILES_PATH))
+
+!--- create a subcommunicator to broadcast the identical mesh and model databases if needed
+    if (BROADCAST_SAME_MESH_AND_MODEL) then
+
+      call MPI_COMM_RANK(MPI_COMM_WORLD,myrank,ier)
+!     to broadcast the model, split along similar ranks per run instead
+      my_group_for_bcast = mod(myrank,NPROC)
+      key = myrank
+      if (my_group_for_bcast < 0 .or. my_group_for_bcast > NPROC-1) stop 'invalid value of my_group_for_bcast'
+
+!     build the sub-communicators
+      call MPI_COMM_SPLIT(MPI_COMM_WORLD, my_group_for_bcast, key, my_local_mpi_comm_for_bcast, ier)
+      if (ier /= 0) stop 'error while trying to create the sub-communicators'
+
+!     see if that process will need to read the mesh and model database and then broadcast it to others
+      call MPI_COMM_RANK(my_local_mpi_comm_for_bcast,my_local_rank_for_bcast,ier)
+      if (my_local_rank_for_bcast > 0) I_should_read_the_database = .false.
+
+    else
+
+! no broadcast of the mesh and model databases to other runs in that case
+      my_group_for_bcast = 0
+      my_local_mpi_comm_for_bcast = MPI_COMM_NULL
+
+    endif
+
+  endif
+
+  end subroutine world_split
+
+!
+!-------------------------------------------------------------------------------------------------
+!
+
+! close sub-communicators if needed, if running more than one earthquake from the same job.
+  subroutine world_unsplit()
+
+  use my_mpi
+  use constants,only: NUMBER_OF_SIMULTANEOUS_RUNS,BROADCAST_SAME_MESH_AND_MODEL
+
+  implicit none
+
+  integer :: ier
+
+  if (NUMBER_OF_SIMULTANEOUS_RUNS > 1) then
+    call MPI_COMM_FREE(my_local_mpi_comm_world,ier)
+    if (BROADCAST_SAME_MESH_AND_MODEL) call MPI_COMM_FREE(my_local_mpi_comm_for_bcast,ier)
+  endif
+
+  end subroutine world_unsplit
 
