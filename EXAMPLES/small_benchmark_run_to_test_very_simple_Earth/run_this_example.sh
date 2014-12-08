0d505ae9902d4a7aabe1d961086675f7aad9162d
Dimitri Komatitsch <komatitsch@lma.cnrs-mrs.fr>
2014-12-09 00:57:56 +0100

    added run_this_example.sh for EXAMPLES/small_benchmark_run_to_test_very_simple_Earth and EXAMPLES/small_benchmark_run_to_test_more_complex_Earth

---
+++ EXAMPLES/small_benchmark_run_to_test_very_simple_Earth/run_this_example.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+#
+# regional simulation example
+#
+# script runs mesher and solver using mpirun
+# on 4 CPUs
+#
+# synthetics have an approximate shortest period ~ 15 s
+#
+# modify accordingly for your own system specifics
+##################################################
+
+echo "running example: `date`"
+currentdir=`pwd`
+
+echo "directory: $currentdir"
+echo "(will take about 15 minutes)"
+echo
+
+# sets up directory structure in current example directoy
+echo
+echo "   setting up example..."
+echo
+
+mkdir -p DATABASES_MPI
+mkdir -p OUTPUT_FILES
+
+rm -rf DATABASES_MPI/*
+rm -rf OUTPUT_FILES/*
+
+# links data directories needed to run example in this current directory with s362ani
+rm -rf DATA
+mkdir -p DATA
+cd DATA/
+ln -s ../Par_file
+ln -s ../CMTSOLUTION
+ln -s ../STATIONS
+ln -s ../../../DATA/crust2.0
+ln -s ../../../DATA/s362ani
+ln -s ../../../DATA/QRFSI12
+ln -s ../../../DATA/topo_bathy
+cd ../
+
+# compiles executables in root directory
+# using default configuration
+cp DATA/Par_file ../../DATA
+cd ../../
+make clean
+make all
+
+# backup of constants setup
+cp setup/* $currentdir/OUTPUT_FILES/
+cp OUTPUT_FILES/values_from_mesher.h $currentdir/OUTPUT_FILES/values_from_mesher.h.compilation
+cp DATA/Par_file $currentdir/OUTPUT_FILES/
+
+cd $currentdir
+
+# copy executables
+mkdir -p bin
+rm -rf bin/*
+cp ../../bin/xmeshfem3D ./bin/
+cp ../../bin/xspecfem3D ./bin/
+cp ../../bin/xcombine_vol_data ./bin/
+
+# run mesher & solver
+echo
+echo "  running script..."
+echo
+./run_mesher_solver.bash
+
+echo `date`
+
