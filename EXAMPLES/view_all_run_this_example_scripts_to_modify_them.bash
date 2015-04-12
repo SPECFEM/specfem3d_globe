db3480ebe88489817913e18ba96c2ee8b4a4658f
Dimitri Komatitsch <komatitsch@lma.cnrs-mrs.fr>
2015-04-12 18:30:25 +0200

    flexible editor selection

---
+++ EXAMPLES/view_all_run_this_example_scripts_to_modify_them.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# this list of files is obtained using:    find . -iname run_this_example.sh -exec ls -1 {} \;
+
+if [ -z "$EDITOR" ]
+then
+EDITOR=vi
+fi
+
+$EDITOR ./small_benchmark_run_to_test_more_complex_Earth/run_this_example.sh ./regional_Greece_noise_small/run_this_example.sh ./global_PREM_kernels/traveltime/run_this_example.sh ./global_PREM_kernels/amplitude/run_this_example.sh ./small_benchmark_run_to_test_very_simple_Earth/run_this_example.sh ./global_s362ani_shakemovie/run_this_example.sh ./regional_MiddleEast/run_this_example.sh ./regional_Greece_small/run_this_example.sh
