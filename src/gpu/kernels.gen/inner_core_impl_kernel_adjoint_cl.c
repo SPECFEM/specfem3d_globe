30375e784668964c2eab59af95f9538ea510530d
Kevin Pouget <kevin.pouget@imag.fr>
2015-04-24 15:07:53 +0200

    regenerate kernels with boast 1.0.1

--- src/gpu/kernels.gen/inner_core_impl_kernel_adjoint_cl.c
+++ src/gpu/kernels.gen/inner_core_impl_kernel_adjoint_cl.c
@@ -1,5 +1,5 @@
 //note: please do not modify this file manually!
-//      this file has been generated automatically by BOAST version 0.99998
+//      this file has been generated automatically by BOAST version 1.0.1
 //      by: make boast_kernels
 
 /*
