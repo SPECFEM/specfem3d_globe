06392a5fab77d4421245394a650b7577d620922c
daniel peter <peterda@ethz.ch>
2015-07-16 02:10:04 +0300

    updates example regional_Greece_noise_small/

--- EXAMPLES/small_benchmark_run_to_test_very_simple_Earth/compare_seismograms.sh
+++ EXAMPLES/small_benchmark_run_to_test_very_simple_Earth/compare_seismograms.sh
@@ -7,6 +7,16 @@ echo
 echo "running seismogram comparisons:"
 echo
 
+# uncompress seismograms
+if [ -e OUTPUT_FILES_reference_OK/II.AAK.MXE.sem.ascii.bz2 ]; then
+  echo
+  echo "unzipping references..."
+  echo
+  bunzip2 OUTPUT_FILES_reference_OK/*.bz2
+  echo
+  echo
+fi
+
 # compares seismograms by plotting correlations
 ../../utils/compare_seismogram_correlations.py OUTPUT_FILES/ OUTPUT_FILES_reference_OK/
 
