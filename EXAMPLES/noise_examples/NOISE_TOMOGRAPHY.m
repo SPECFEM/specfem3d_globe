9b02e1a7ae8e2705a3a5944210ae7b4bd3a95b73
daniel peter <peterda@ethz.ch>
2015-07-31 17:30:44 +0300

    updates matlab script NOISE_TOMOGRAPHY.m to work with large number of time steps

--- EXAMPLES/noise_examples/NOISE_TOMOGRAPHY.m
+++ EXAMPLES/noise_examples/NOISE_TOMOGRAPHY.m
@@ -56,8 +56,10 @@ taper_length_min = 40;         % minimum number of steps for tapering ends
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %%
-clf;
-fontsize=10;       % fontsize for figures
+if show_figures
+  clf;
+  fontsize=10;       % fontsize for figures
+end
 
 %% derived parameters
 T=(NSTEP-1)*dt;    % total simulation time
@@ -93,49 +95,6 @@ displ=zeros(size(f)); % for displacement
 filter_array=zeros(size(f)); % for constraining frequencies within ranges
                              % defined by Tmin & Tmax
 
-%% taper
-if taper_type==1
-  % determines taper length
-  taper_length = round(taper_length_percentage * NSTEP);
-  if taper_length < taper_length_min
-    taper_length = taper_length_min;
-  end
-  fprintf('\n  using taper (cosine) length: %i \n',taper_length);
-  % check size
-  if NSTEP <= 2*taper_length
-    fprintf('\nError number of time steps = %i must be bigger than taper window size = %i ! Please retry... \n',NSTEP,2*taper_length+1);
-    exit(1);
-  end
-
-  % cosine taper (both branches, value 0 at index 1, value 1 at index length+1, value 0 at 2*length+1
-  taper=zeros(2*taper_length+1);
-  for l=1:2*taper_length+1
-    taper(l) = (1.0-cos(pi*2.0*(l-1)/(2*taper_length)))/2.0;
-  end
-
-  % sets up window function for multiplication with signal
-  window=ones(NSTEP);
-  % adds increasing branch
-  window(1:taper_length) = taper(1:taper_length);
-  % adds decreasing branch
-  window(NSTEP-taper_length+1:NSTEP) = taper(taper_length+2:2*taper_length+1);
-
-  % plots figure
-  if show_figures
-    fprintf('  plotting figure: Taper and Taper window \n');
-    figure(2);
-    subplot(2,1,1);
-    plot(taper);hold on;
-    xlabel('steps','fontsize',fontsize);ylabel('Taper','fontsize',fontsize);
-    title('Taper','fontsize',fontsize);
-    subplot(2,1,2);
-    plot(window);
-    xlabel('steps','fontsize',fontsize);ylabel('Taper window','fontsize',fontsize);
-    title('Window','fontsize',fontsize);
-    drawnow;
-  end
-end
-
 %% calculate the power spectrum of noise from Peterson's model (1993)
 fprintf('\n  calculating noise power spectrum: number of positive frequencies = %i \n',N_mid);
 % only calculate for positive frequencies
@@ -166,7 +125,7 @@ for l=1:N_mid
 end
 
 %% constrain the power spectrum only within the range [Tmin Tmax]
-fprintf('\n  filtering power spectrum:\n    period     min/max = %f / %f\n    frequency min/max = %f / %f \n',Tmin,Tmax,1/Tmax,1/Tmin);
+fprintf('\n  filtering power spectrum:\n    period     min/max = %f / %f\n    frequency  min/max = %f / %f \n',Tmin,Tmax,1/Tmax,1/Tmin);
 for l=1:N_mid
     if abs(f(l))>=1/Tmax && abs(f(l))<=1/Tmin
         filter_array(l)=sin((f(l)-1/Tmax)/(1/Tmin-1/Tmax)*pi);
@@ -191,16 +150,20 @@ if show_figures
   xlim([0.8/Tmax 1.2/Tmin]);ylim([-0.1 1.5]);
 end
 
+% using only displacement from here on
+clear accel;
+clear veloc;
+
 %% update power spectrum in the negative frequencies, using symmetry
 % note the power spectrum is always REAL, instead of COMPLEX
 for l=N_mid+1:NSTEP
-    accel(l)=conj(accel(NSTEP-l+2));
-    veloc(l)=conj(veloc(NSTEP-l+2));
+    %accel(l)=conj(accel(NSTEP-l+2));
+    %veloc(l)=conj(veloc(NSTEP-l+2));
     displ(l)=conj(displ(NSTEP-l+2));
 end
 
 %% prepare source time function for ensemble forward source -- S_squared
-fprintf('\n  preparing source time function S_squared: NSTEP = %i / dt = %f \n',NSTEP,dt);
+fprintf('\n  preparing source time function S_squared:\n    NSTEP = %i / dt = %f \n',NSTEP,dt);
 
 % the file S_squared should be put into directory ./NOISE_TOMOGRAPHY/
 % together with other two files: irec_master_noise & nu_master
@@ -218,8 +181,61 @@ temp(1:N_mid-1)  =S_squared(N_mid+1:NSTEP,2);
 % tapers ends
 if taper_type==1
   fprintf('\n  tapering source time function S_squared\n');
-  for l=1:NSTEP
-    temp(l) = temp(l) * window(l);
+
+  % determines taper length
+  taper_length = round(taper_length_percentage * NSTEP);
+  if taper_length < taper_length_min
+    taper_length = taper_length_min;
+  end
+  fprintf('  using taper (cosine) length: %i \n',taper_length);
+  % check size
+  if NSTEP <= 2*taper_length
+    fprintf('\nError number of time steps = %i must be bigger than taper window size = %i ! Please retry... \n',NSTEP,2*taper_length+1);
+    exit(1);
+  end
+
+  % cosine taper (both branches, value 0 at index 1, value 1 at index length+1, value 0 at 2*length+1
+  taper=zeros(2*taper_length+1);
+  for l=1:2*taper_length+1
+    taper(l) = (1.0-cos(pi*2.0*(l-1)/(2*taper_length)))/2.0;
+  end
+
+  % sets up window function for multiplication with signal
+  % note: this becomes too memory intensive for large number of time steps, using taper directly
+  %window=ones(NSTEP);
+  %% adds increasing branch
+  %window(1:taper_length) = taper(1:taper_length);
+  %% adds decreasing branch
+  %window(NSTEP-taper_length+1:NSTEP) = taper(taper_length+2:2*taper_length+1);
+  %  
+  %for l=1:NSTEP
+  %  temp(l) = temp(l) * window(l);
+  %end
+
+  % applies taper
+  for l=1:taper_length
+    % increasing branch
+    temp(l) = temp(l) * taper(l);
+    % decreasing branch
+    k = NSTEP-taper_length+l;
+    temp(k) = temp(k) * taper(taper_length+1+l);
+  end
+
+  % plots figure
+  if show_figures
+    fprintf('  plotting figure: Taper and Taper window \n');
+    figure(2);
+    % taper
+    subplot(2,1,1);
+    plot(taper);hold on;
+    xlabel('steps','fontsize',fontsize);ylabel('Taper','fontsize',fontsize);
+    title('Taper','fontsize',fontsize);
+    % taper window
+    %subplot(2,1,2);
+    %plot(window);
+    %xlabel('steps','fontsize',fontsize);ylabel('Taper window','fontsize',fontsize);
+    %title('Window','fontsize',fontsize);
+    drawnow;
   end
 end
 
