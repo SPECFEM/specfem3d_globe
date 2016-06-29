4a1f078e4a69c3775e95651ecb2ba5852b3cca89
daniel peter <daniel.peter22@gmail.com>
2016-06-29 11:37:10 +0300

    adds tests directory

---
+++ tests/compilations/1.configure.openmp_make.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+###################################################
+
+# test name
+NAME="1.configure.openmp_make"
+
+# configuration parameters
+CONF_PARAM="--enable-openmp --enable-vectorization"
+
+###################################################
+
+
+testdir=`pwd`
+
+# sets source directory
+cd $ROOT/
+srcdir=`pwd`
+
+cd $testdir/
+
+# title
+echo >> $testdir/results.log
+echo "$NAME in: $testdir" >> $testdir/results.log
+echo >> $testdir/results.log
+
+#cleanup
+rm -rf config.log config.status
+rm -rf ./bin ./obj ./setup ./OUTPUT_FILES ./DATA
+
+# configuration
+# (out-of-source compilation)
+echo "configuration: $srcdir/configure ${CONF_PARAM}" >> $testdir/results.log
+$srcdir/configure ${CONF_PARAM} >> $testdir/results.log 2>&1
+
+# checks exit code
+if [[ $? -ne 0 ]]; then
+  echo >> $testdir/results.log
+  echo "configuration failed, please check..." >> $testdir/results.log
+  exit 1
+fi
+
+# default all compilation
+make clean >> $testdir/results.log 2>&1
+
+# checks exit code
+if [[ $? -ne 0 ]]; then
+  echo >> $testdir/results.log
+  echo "compilation failed, please check..." >> $testdir/results.log
+  exit 1
+fi
+
+# parallel make
+make -j 4 all >> $testdir/results.log 2>&1
+
+# checks exit code
+if [[ $? -ne 0 ]]; then
+  echo >> $testdir/results.log
+  echo "compilation failed, please check..." >> $testdir/results.log
+  exit 1
+fi
+
+echo "" >> $testdir/results.log
+echo "successful compilation" >> $testdir/results.log
+
---
+++ tests/examples/1.run_regional_Greece_small.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+###################################################
+
+# example directory name
+NAME="regional_Greece_small"
+
+# relative location of SPECFEM3D_GLOBE EXAMPLES/ directory for tests directories (e.g. SPECFEM3D_GLOBE/tests/examples)
+EXAMPLES="../../EXAMPLES/"
+
+###################################################
+
+# bash function for checking seismogram output with reference solutions
+my_test(){
+  echo "testing seismograms:";
+  ../../utils/compare_seismogram_correlations.py OUTPUT_FILES/ REF_SEIS/;
+  ../../utils/compare_seismogram_correlations.py OUTPUT_FILES/ REF_SEIS/ | grep min/max | cut -d \| -f 3 | awk '{print "correlation:",$1; if ($1 < 0.9 ){print $1,"failed"; exit 1;}else{ print $1,"good"; exit 0;}}';
+}
+
+testdir=`pwd`
+
+# title
+echo >> $testdir/results.log
+echo "$NAME in: $testdir" >> $testdir/results.log
+echo >> $testdir/results.log
+
+# checks if example directory exists
+if [ ! -e $EXAMPLES/$NAME/run_this_example.sh ]; then
+  echo "run-script in directory $EXAMPLES/$NAME not found, please check..." >> $testdir/results.log
+  exit 1
+fi
+
+#cleanup output
+rm -rf ./OUTPUT_FILES ./DATA ./run_this_example.sh ./run_mesher_solver.bash ./REF_SEIS
+mkdir -p OUTPUT_FILES
+
+# setup
+cp -rp $EXAMPLES/$NAME/DATA .
+cp -p $EXAMPLES/$NAME/run_this_example.sh .
+cp -p $EXAMPLES/$NAME/run_mesher_solver.bash .
+ln -s $EXAMPLES/$NAME/REF_SEIS
+
+# makes sure only 1 x 2 x 2 procs in total are used
+#sed -i "s:^NPROC_XI .*:NPROC_XI      = 2:" DATA/Par_file
+#sed -i "s:^NPROC_ETA .*:NPROC_ETA    = 2:" DATA/Par_file
+# limits length
+sed -i "s:^RECORD_LENGTH_IN_MINUTES .*:RECORD_LENGTH_IN_MINUTES    = 0.3:" DATA/Par_file
+
+# checks exit code
+if [[ $? -ne 0 ]]; then
+  echo >> $testdir/results.log
+  echo "setup failed, please check..." >> $testdir/results.log
+  exit 1
+fi
+
+./run_this_example.sh >> $testdir/results.log 2>&1
+
+# checks exit code
+if [[ $? -ne 0 ]]; then
+  echo >> $testdir/results.log
+  echo "run failed, please check..." >> $testdir/results.log
+  exit 1
+fi
+
+# test seismograms
+my_test >> $testdir/results.log
+
+# checks exit code
+if [[ $? -ne 0 ]]; then
+  echo >> $testdir/results.log
+  echo "seismograms failed, please check..." >> $testdir/results.log
+  exit 1
+fi
+
+# cleanup
+rm -rf ./OUTPUT_FILES ./DATA ./run_this_example.sh ./REF_SEIS
+
+echo "successful compilation" >> $testdir/results.log
+
