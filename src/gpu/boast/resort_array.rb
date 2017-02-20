0210d8fefd0a763e3357c64ec5d71624bb6c3754
Malte Schirwon <malte.schirwon@mathematik.uni-stuttgart.de>
2017-02-20 10:08:21 +0100

    added new files for resorting
    Signed-off-by: Malte Schirwon <malte.schirwon@mathematik.uni-stuttgart.de>

---
+++ src/gpu/boast/resort_array.rb
@@ -0,0 +1,55 @@
+module BOAST
+
+  def BOAST::resort_array(ref = true, n_gll3 = 125)
+    push_env( :array_start => 0 )
+    kernel = CKernel::new
+    function_name = "resort_array"
+
+    v = []
+    v.push old_array             = Real("old_array",             :dir => :inout,:dim => [Dim()] )
+
+    ngll3 = Int("NGLL3", :const => n_gll3)
+    
+    p = Procedure(function_name, v)
+    
+    if (get_lang == CUDA and ref) then
+      get_output.print File::read("references/#{function_name}.cu")
+    elsif(get_lang == CL or get_lang == CUDA) then
+      make_specfem3d_header( :ngll3 => n_gll3 )
+
+      open p
+        decl ispec = Int("ispec")
+        decl i = Int("i")
+        decl id = Int("id")
+        decl idx = Int("idx")
+        decl t_idx = Int("t_idx")
+        decl tx = Int("tx")
+        decl offset = Int("offset")
+        decl sh_tmp = Real("sh_tmp",     :local => true, :dim => [Dim(21*n_gll3)] )
+        
+        print ispec === get_group_id(0) + get_group_id(1)*get_num_groups(0)
+        print tx === get_local_id(0)
+        
+        print offset === ispec*ngll3*21+tx
+
+        print For(i, 0, 21-1) {
+            print sh_tmp[i*ngll3+tx] === old_array[i*ngll3+offset]
+        }
+        # synchronizes threads
+        print barrier(:local)
+
+        print For(i, 0, 21-1) {
+        print id === (i*ngll3+tx)
+        print idx === id / 21
+        print t_idx === Modulo(id, 21)
+        print old_array[i*ngll3+offset] === sh_tmp[idx + t_idx*ngll3 ]
+        }
+      close p
+    else
+      raise "Unsupported language!"
+    end
+    pop_env( :array_start )
+    kernel.procedure = p
+    return kernel
+  end
+end
---
+++ src/gpu/kernels.gen/resort_array_cl.c
@@ -0,0 +1,121 @@
+//note: please do not modify this file manually!
+//      this file has been generated automatically by BOAST version 2.0.1
+//      by: make boast_kernels
+
+/*
+!=====================================================================
+!
+!          S p e c f e m 3 D  G l o b e  V e r s i o n  7 . 0
+!          --------------------------------------------------
+!
+!     Main historical authors: Dimitri Komatitsch and Jeroen Tromp
+!                        Princeton University, USA
+!                and CNRS / University of Marseille, France
+!                 (there are currently many more authors!)
+! (c) Princeton University and CNRS / University of Marseille, April 2014
+!
+! This program is free software; you can redistribute it and/or modify
+! it under the terms of the GNU General Public License as published by
+! the Free Software Foundation; either version 2 of the License, or
+! (at your option) any later version.
+!
+! This program is distributed in the hope that it will be useful,
+! but WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+! GNU General Public License for more details.
+!
+! You should have received a copy of the GNU General Public License along
+! with this program; if not, write to the Free Software Foundation, Inc.,
+! 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+!
+!=====================================================================
+*/
+
+const char * resort_array_program = "\
+inline void atomicAdd(volatile __global float *source, const float val) {\n\
+  union {\n\
+    unsigned int iVal;\n\
+    float fVal;\n\
+  } res, orig;\n\
+  do {\n\
+    orig.fVal = *source;\n\
+    res.fVal = orig.fVal + val;\n\
+  } while (atomic_cmpxchg((volatile __global unsigned int *)source, orig.iVal, res.iVal) != orig.iVal);\n\
+}\n\
+#ifndef INDEX2\n\
+#define INDEX2(isize,i,j) i + isize*j\n\
+#endif\n\
+#ifndef INDEX3\n\
+#define INDEX3(isize,jsize,i,j,k) i + isize*(j + jsize*k)\n\
+#endif\n\
+#ifndef INDEX4\n\
+#define INDEX4(isize,jsize,ksize,i,j,k,x) i + isize*(j + jsize*(k + ksize*x))\n\
+#endif\n\
+#ifndef INDEX5\n\
+#define INDEX5(isize,jsize,ksize,xsize,i,j,k,x,y) i + isize*(j + jsize*(k + ksize*(x + xsize*y)))\n\
+#endif\n\
+\n\
+#ifndef NDIM\n\
+#define NDIM 3\n\
+#endif\n\
+#ifndef NGLLX\n\
+#define NGLLX 5\n\
+#endif\n\
+#ifndef NGLL2\n\
+#define NGLL2 25\n\
+#endif\n\
+#ifndef NGLL3\n\
+#define NGLL3 125\n\
+#endif\n\
+#ifndef NGLL3_PADDED\n\
+#define NGLL3_PADDED 128\n\
+#endif\n\
+#ifndef N_SLS\n\
+#define N_SLS 3\n\
+#endif\n\
+#ifndef IREGION_CRUST_MANTLE\n\
+#define IREGION_CRUST_MANTLE 1\n\
+#endif\n\
+#ifndef IREGION_INNER_CORE\n\
+#define IREGION_INNER_CORE 3\n\
+#endif\n\
+#ifndef IFLAG_IN_FICTITIOUS_CUBE\n\
+#define IFLAG_IN_FICTITIOUS_CUBE 11\n\
+#endif\n\
+#ifndef R_EARTH_KM\n\
+#define R_EARTH_KM 6371.0f\n\
+#endif\n\
+#ifndef COLORING_MIN_NSPEC_INNER_CORE\n\
+#define COLORING_MIN_NSPEC_INNER_CORE 1000\n\
+#endif\n\
+#ifndef COLORING_MIN_NSPEC_OUTER_CORE\n\
+#define COLORING_MIN_NSPEC_OUTER_CORE 1000\n\
+#endif\n\
+#ifndef BLOCKSIZE_TRANSFER\n\
+#define BLOCKSIZE_TRANSFER 256\n\
+#endif\n\
+\n\
+__kernel void resort_array(__global float * old_array){\n\
+  int ispec;\n\
+  int i;\n\
+  int id;\n\
+  int idx;\n\
+  int t_idx;\n\
+  int tx;\n\
+  int offset;\n\
+  __local float sh_tmp[(2625)];\n\
+  ispec = get_group_id(0) + (get_group_id(1)) * (get_num_groups(0));\n\
+  tx = get_local_id(0);\n\
+  offset = ((ispec) * (NGLL3)) * (21) + tx;\n\
+  for (i = 0; i <= 20; i += 1) {\n\
+    sh_tmp[(i) * (NGLL3) + tx] = old_array[(i) * (NGLL3) + offset];\n\
+  }\n\
+  barrier(CLK_LOCAL_MEM_FENCE);\n\
+  for (i = 0; i <= 20; i += 1) {\n\
+    id = (i) * (NGLL3) + tx;\n\
+    idx = (id) / (21);\n\
+    t_idx = ((id < 0) ^ (21 < 0) ? (id % 21) + 21 : id % 21);\n\
+    old_array[(i) * (NGLL3) + offset] = sh_tmp[idx + (t_idx) * (NGLL3)];\n\
+  }\n\
+}\n\
+";
