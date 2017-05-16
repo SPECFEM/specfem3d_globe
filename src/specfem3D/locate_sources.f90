d2e0765c0e9f864bfb393fd8165fd402b1123fc0
buildbot <buildbot@seismobot.Princeton.EDU>
2017-05-16 12:52:56 -0400

    Buildbot ran the code beautifier.

--- src/specfem3D/locate_sources.f90
+++ src/specfem3D/locate_sources.f90
@@ -321,14 +321,14 @@
         phin=stazi*DEGREES_TO_RADIANS
 
         ! we use the same convention as in Harvard normal modes for the orientation
-        
+
         !   vertical component
         n(1) = dcos(thetan)
         !   N-S component
         n(2) = - dsin(thetan)*dcos(phin)
         !   E-W component
         n(3) = dsin(thetan)*dsin(phin)
-        
+
         !   get the Cartesian components of n in the model: nu
         nu_source(iorientation,1,isource) = n(1)*st*cp+n(2)*ct*cp-n(3)*sp
         nu_source(iorientation,2,isource) = n(1)*st*sp+n(2)*ct*sp+n(3)*cp
