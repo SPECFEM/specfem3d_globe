55717b10560bee7c8657ef3245dcbf5c54304664
1sbeller <1sbeller@users.noreply.github.com>
2020-06-19 14:12:13 -0400

    Update get_model.F90
    Add absolute value of argument to avoid square root of slightly negative values.

--- src/meshfem3D/get_model.F90
+++ src/meshfem3D/get_model.F90
@@ -166,8 +166,8 @@
 
         ! stores isotropic shear modulus from reference 1D model
         ! calculates isotropic value
-        vs = sqrt(((1.d0-2.d0*eta_aniso)*vph*vph + vpv*vpv &
-                  + 5.d0*vsh*vsh + (6.d0+4.d0*eta_aniso)*vsv*vsv)/15.d0)
+        vs = sqrt(abs((1.d0-2.d0*eta_aniso)*vph*vph + vpv*vpv &
+                  + 5.d0*vsh*vsh + (6.d0+4.d0*eta_aniso)*vsv*vsv)/15.d0))
         ! stores 1D isotropic mu0 = (rho * Vs*Vs) values
         mu0 = rho * vs*vs
         mu0store(i,j,k,ispec) = real( mu0, kind=CUSTOM_REAL)
