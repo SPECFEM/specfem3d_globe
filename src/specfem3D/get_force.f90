f3a70577234bafa2cae5a4d36d30850f8ea8c9a5
Daniel Peter <daniel.peter22@gmail.com>
2020-09-15 11:45:33 +0300

    updates version number to version 8.0

--- src/specfem3D/get_force.f90
+++ src/specfem3D/get_force.f90
@@ -1,13 +1,13 @@
 !=====================================================================
 !
-!               S p e c f e m 3 D  V e r s i o n  3 . 0
-!               ---------------------------------------
+!          S p e c f e m 3 D  G l o b e  V e r s i o n  8 . 0
+!          --------------------------------------------------
 !
 !     Main historical authors: Dimitri Komatitsch and Jeroen Tromp
 !                        Princeton University, USA
 !                and CNRS / University of Marseille, France
 !                 (there are currently many more authors!)
-! (c) Princeton University and CNRS / University of Marseille, July 2012
+! (c) Princeton University and CNRS / University of Marseille, April 2014
 !
 ! This program is free software; you can redistribute it and/or modify
 ! it under the terms of the GNU General Public License as published by
