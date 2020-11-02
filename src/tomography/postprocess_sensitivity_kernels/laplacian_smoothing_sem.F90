3dd9f6f300b9aecd08934aeafbe3ef9df8bef1ba
Congyue Cui <ccui@princeton.edu>
2020-11-01 22:52:52 -0500

    Fixed an uninitialized variable in xsmooth_sem

--- src/tomography/postprocess_sensitivity_kernels/laplacian_smoothing_sem.F90
+++ src/tomography/postprocess_sensitivity_kernels/laplacian_smoothing_sem.F90
@@ -590,6 +590,7 @@ program smooth_laplacian_sem
      close(IIN)
 #endif
      ! Get normalization factor
+     norm_ker = 0.
      norm_kerl = sum(mo**2)/nglob
      call sum_all_cr(norm_kerl, norm_ker)
      norm_ker = sqrt(norm_ker)
