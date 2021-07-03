210136210d52143919916d761d7887c6b522e6f1
Daniel Peter <daniel.peter22@gmail.com>
2021-07-03 23:22:03 +0200

    updates travis/actions

---
+++ .travis/run_build.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+#
+# builds all executables
+#
+
+# getting updated environment (CUDA_HOME, PATH, ..)
+if [ -f $HOME/.tmprc ]; then source $HOME/.tmprc; fi
+
+# checks if anything to do
+echo "run checks: $RUN_CHECKS"
+if [ "$RUN_CHECKS" == "0" ]; then
+  echo "  no run checks required, exiting..."
+  exit 0
+else
+  echo "  run checks required, start testing..."
+fi
+echo
+
+# info
+echo $WORKDIR
+echo
+echo "**********************************************************"
+echo
+echo "configuration test: TESTID=${TESTID} TESTDIR=${TESTDIR} TESTCOV=${TESTCOV} TESTFLAGS=${TESTFLAGS}"
+echo
+echo "**********************************************************"
+echo
+
+
+###########################################################
+# configuration & compilation
+###########################################################
+# configuration
+echo 'Configure...' && echo -en 'travis_fold:start:configure\\r'
+echo "configuration:"
+
+if [ "$TESTCOV" == "1" ]; then
+  echo "configuration: for coverage"
+  ./configure FC=${FC} MPIFC=${MPIFC} CC=${CC} ${TESTFLAGS} FLAGS_CHECK="-fprofile-arcs -ftest-coverage -O0" CFLAGS="-coverage -O0"
+else
+  if [ "$CUDA" == "true" ]; then
+    if [ "$OPENCL" == "true" ]; then
+      echo "configuration: for opencl" # uses libOpenCL provided from CUDA package
+      ./configure FC=${FC} MPIFC=${MPIFC} CC=${CC} ${TESTFLAGS} OCL_CPU_FLAGS="-g -Wall -std=c99 -DWITH_MPI" OCL_GPU_FLAGS="-Werror" OCL_INC="${CUDA_HOME}/include" OCL_LIB="${CUDA_HOME}/lib64" OCL_LIBS="-lOpenCL"
+    else
+      echo "configuration: for cuda"
+      ./configure FC=${FC} MPIFC=${MPIFC} CC=${CC} ${TESTFLAGS} CUDA_LIB="${CUDA_HOME}/lib64" CUDA_INC="${CUDA_HOME}/include" CUDA_FLAGS="-Xcompiler -Wall,-Wno-unused-function,-Wno-unused-const-variable,-Wfatal-errors -g -G"
+    fi
+  else
+    echo "configuration: default"
+    ./configure FC=${FC} MPIFC=${MPIFC} CC=${CC} ${TESTFLAGS}
+  fi
+fi
+if [[ $? -ne 0 ]]; then echo "configuration failed:"; cat config.log; echo ""; echo "exiting..."; exit 1; fi
+
+# we output to console
+sed -i "s:IMAIN .*:IMAIN = ISTANDARD_OUTPUT:" setup/constants.h
+
+# regional w/ NGLL = 6
+if [ "$TESTID" == "8" ]; then
+  sed -i "s:NGLLX =.*:NGLLX = 6:" setup/constants.h
+fi
+
+echo -en 'travis_fold:end:configure\\r'
+
+# compilation  (only cleaning)
+echo 'Build...' && echo -en 'travis_fold:start:build\\r'
+echo "compilation:"
+
+make clean
+
+if [[ $? -ne 0 ]]; then exit 1; fi
+echo -en 'travis_fold:end:build\\r'
+
+echo
+echo "done "
+echo `date`
+echo
--- .travis/run_tests.sh
+++ .travis/run_tests.sh
@@ -3,6 +3,15 @@
 # getting updated environment (CUDA_HOME, PATH, ..)
 if [ -f $HOME/.tmprc ]; then source $HOME/.tmprc; fi
 
+# checks if anything to do
+echo "run checks: $RUN_CHECKS"
+if [ "$RUN_CHECKS" == "0" ]; then
+  echo "  no run checks required, exiting..."
+  exit 0
+else
+  echo "  run checks required, start testing..."
+fi
+echo
 
 ###########################################################
 # setup
@@ -27,12 +36,13 @@ esac
 
 
 # info
-echo $TRAVIS_BUILD_DIR
+#echo $TRAVIS_BUILD_DIR
 echo $WORKDIR
+echo `date`
 echo
 echo "**********************************************************"
 echo
-echo "configuration test: TESTID=${TESTID} TESTDIR=${TESTDIR} TESTCOV=${TESTCOV} TESTFLAGS=${TESTFLAGS}"
+echo "run test: TESTID=${TESTID} TESTDIR=${TESTDIR} TESTCOV=${TESTCOV} "
 echo
 echo "    test directory: $dir"
 echo
@@ -58,55 +68,13 @@ my_report(){
 }
 
 
-
-###########################################################
-# configuration & compilation
-###########################################################
-# configuration
-echo 'Configure...' && echo -en 'travis_fold:start:configure\\r'
-echo "configuration:"
-
-if [ "$TESTCOV" == "1" ]; then
-  echo "configuration: for coverage"
-  ./configure FC=${FC} MPIFC=${MPIFC} CC=${CC} ${TESTFLAGS} FLAGS_CHECK="-fprofile-arcs -ftest-coverage -O0" CFLAGS="-coverage -O0"
-else
-  if [ "$CUDA" == "true" ]; then
-    if [ "$OPENCL" == "true" ]; then
-      echo "configuration: for opencl" # uses libOpenCL provided from CUDA package
-      ./configure FC=${FC} MPIFC=${MPIFC} CC=${CC} ${TESTFLAGS} OCL_CPU_FLAGS="-g -Wall -std=c99 -DWITH_MPI" OCL_GPU_FLAGS="-Werror" OCL_INC="${CUDA_HOME}/include" OCL_LIB="${CUDA_HOME}/lib64" OCL_LIBS="-lOpenCL"
-    else
-      echo "configuration: for cuda"
-      ./configure FC=${FC} MPIFC=${MPIFC} CC=${CC} ${TESTFLAGS} CUDA_LIB="${CUDA_HOME}/lib64" CUDA_INC="${CUDA_HOME}/include" CUDA_FLAGS="-Xcompiler -Wall,-Wno-unused-function,-Wno-unused-const-variable,-Wfatal-errors -g -G"
-    fi
-  else
-    echo "configuration: default"
-    ./configure FC=${FC} MPIFC=${MPIFC} CC=${CC} ${TESTFLAGS}
-  fi
-fi
-if [[ $? -ne 0 ]]; then echo "configuration failed:"; cat config.log; echo ""; echo "exiting..."; exit 1; fi
-
-# we output to console
-sed -i "s:IMAIN .*:IMAIN = ISTANDARD_OUTPUT:" setup/constants.h
-
-# regional w/ NGLL = 6
-if [ "$TESTID" == "8" ]; then
-  sed -i "s:NGLLX =.*:NGLLX = 6:" setup/constants.h
-fi
-echo -en 'travis_fold:end:configure\\r'
-
-# compilation  (only cleaning)
-echo 'Build...' && echo -en 'travis_fold:start:build\\r'
-echo "compilation:"
-make clean
-if [[ $? -ne 0 ]]; then exit 1; fi
-echo -en 'travis_fold:end:build\\r'
-
-
 ###########################################################
 # test examples
 ###########################################################
+
 # testing internal mesher example (short & quick for all configuration)
 echo 'Tests...' && echo -en 'travis_fold:start:tests\\r'
+
 # runs test
 echo "test directory: $dir"
 echo
@@ -161,20 +129,36 @@ else
     exit 1
   fi
 
+  # simulation done
+  echo
+  echo "simulation done: `pwd`"
+  echo `date`
+  echo
+
   # seismogram comparison
   if [ "$TESTCOV" == "0" ] && [ ! "$TESTID" == "7" ] && [ ! "$TESTID" == "8" ] && [ ! "$TESTID" == "21" ]; then
     my_test
   fi
-  cd $WORKDIR
 fi
-if [[ $? -ne 0 ]]; then exit 1; fi
-echo -en 'travis_fold:end:tests\\r'
 
+#checks
+if [[ $? -ne 0 ]]; then exit 1; fi
+
+# simulation done
+echo
+echo "test done: `pwd`"
+echo `date`
+echo
+
+echo -en 'travis_fold:end:tests\\r'
+echo
 
 # code coverage: https://codecov.io/gh/geodynamics/specfem3d/
 # additional runs for coverage
 #
 # note: log becomes too long, trying to fold each test output
+cd $WORKDIR
+
 echo 'Coverage...' && echo -en 'travis_fold:start:coverage.point-force\\r'
 if [ "$TESTCOV" == "1" ] && [ "$TESTID" == "1" ]; then
   ##
@@ -331,5 +315,6 @@ echo -en 'travis_fold:end:coverage.regional-noise\\r'
 
 
 # done
-echo "done `pwd`"
-
+echo "all done"
+echo `date`
+echo
